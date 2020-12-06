98c32c1e84293ff83d6dfd78093c37f1916979bf
Seth Whitaker <seth.whitaker@snhu.edu>
2020-12-06 15:13:12 -0700

    Initial Commit

---
+++ EnhancementOne/LiarsDiceEnchancements/LiarsDiceEnchancements/Models/Die.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiarsDiceEnhancements.Models
+{
+    class Die
+    {
+        public int DieValue { get; set; }
+
+        public Die()
+        {
+
+        }
+    }
+}
